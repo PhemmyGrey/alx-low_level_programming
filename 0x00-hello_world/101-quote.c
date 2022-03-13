77721aab0f2a39de1cba079c4c66a34c58d489db
PhemmyGrey <femipics@yahoo.com>
2022-03-13 08:22:52 -0700

    last always hardest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,14 @@
  *
  * Description: Print to stout without using put or printf
  * Description: Print to stderr without usig puts or printf
- * Return: return 1 
+ * Return: If all goes on, it return 1
  */
 int main(void)
 {
-	char strng[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char strng[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 	int nbytes = strlen(strng);
 	write(2, strng, nbytes);
 	return (1);
 }
+
 
