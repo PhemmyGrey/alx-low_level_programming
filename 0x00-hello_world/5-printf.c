3532db7edcb4d4eff8cc144c866b1bce4966e723
PhemmyGrey <femipics@yahoo.com>
2022-03-13 06:37:02 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main program
+ *
+ * Description: Using Printf on alx
+ * Return: If correct, it returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
