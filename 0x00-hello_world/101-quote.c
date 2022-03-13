b4d9e33f651bd2cb4363e0631e27863470f6b081
PhemmyGrey <femipics@yahoo.com>
2022-03-13 08:03:09 -0700

    last question

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *
+ * Description: Print to stout without using put or printf
+ * Description: Print to stderr without usig puts or printf
+ * Return: return 1 
+ */
+int main(void)
+{
+	char strng[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int nbytes = strlen(strng);
+	write(2, strng, nbytes);
+	return (1);
+}
+
