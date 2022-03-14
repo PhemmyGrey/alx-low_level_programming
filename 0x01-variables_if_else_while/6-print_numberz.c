d89b8f50a661a02396d31e247dae8b1314cb8abc
PhemmyGrey <femipics@yahoo.com>
2022-03-14 02:26:02 -0700

    a couple more

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main Project
+ * Description: Print all numbers base 10 from 0
+ * do not use any variable of type char
+ * use 'putcha' to print console and use only twice
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
