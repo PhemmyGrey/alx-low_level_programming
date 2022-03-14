6bce2494bebe90c5469e5879ebc58f0685e7b246
PhemmyGrey <femipics@yahoo.com>
2022-03-14 03:38:23 -0700

    all weekend work

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,15 +2,16 @@
 /**
  * main - main project
  * Description:all possible single digit number combination.
+ * use 'putchar' to print console
  * using only 'putchar' four times at most
  * do not use any 'char' variable
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i = 0;
 
-	while (i < 0)
+	while (i < 10)
 	{
 		putchar(i + '0');
 		if (i < 9)
