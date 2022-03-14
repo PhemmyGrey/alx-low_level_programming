c2ada7c84c67e612ff1ebc0e2e4db7d8e7fe8b53
PhemmyGrey <femipics@yahoo.com>
2022-03-14 01:08:44 -0700

    lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - My Program
+ *
+ * Description: Display lower case alphabets , followed by a new line. 
+ * Return: if right , it returns 0
+ */
+int main(void)
+{
+	char 1char;
+
+	for (1char = 'a'; 1char <= 'z'; 1char++)
+		putchar(1char);
+
+	putchar(1char);
+
+	putchar('\n');
+
+	return (0);
+
+}
+
