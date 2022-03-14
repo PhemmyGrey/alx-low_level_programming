5f4dd8962ee36306cbfe337774100ea5c0346f40
PhemmyGrey <femipics@yahoo.com>
2022-03-14 01:44:21 -0700

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio>
+/**
+ * main - main project
+ * Description: using 'putchar' for lower and uppercase alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c<= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
+
