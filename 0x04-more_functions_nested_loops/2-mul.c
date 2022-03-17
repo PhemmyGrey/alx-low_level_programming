e388f694349021fb5eef6a8eda4f327a2e4f6727
PhemmyGrey <femipics@yahoo.com>
2022-03-17 21:47:33 +0800

    my internet is so bad today

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two digits
+ *
+ * @a: fist number
+ *
+ * @b: second number
+ *
+ * Return: result of multiplier
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
