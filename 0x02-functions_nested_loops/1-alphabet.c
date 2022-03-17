5a334b6d01487029ec2ee99857d7d75e9ad03f0f
PhemmyGrey <femipics@yahoo.com>
2022-03-17 17:07:54 +0800

    my commit for task functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Description - prints lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char abc;
+
+	abc = 'a';
+
+	while (abc <= 'z')
+	{
+		_putchar(p);
+		abc++;
+	}
+	_putchar('\n');
+
+}
