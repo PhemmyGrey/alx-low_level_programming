cbdbf51e61bc59d99621ee9d00aa754f5b8f5047
PhemmyGrey <femipics@yahoo.com>
2022-03-18 07:00:18 +0800

    nested func

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function that prints the numbers,from 0 to 9. exclude 2, 4
+ * print_most_numbers - print the numbers, from 0 to 9. without 2, 4
  */
 
 void print_most_numbers(void)
