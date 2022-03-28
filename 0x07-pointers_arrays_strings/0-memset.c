c2c70e34669b78737391e5c367db32feadbef613
PhemmyGrey <femipics@yahoo.com>
2022-03-28 23:37:25 +0800

    tasks for today-s arrays pointers and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_memset - fill memory with constant value
+ *@s: pointer
+ *@b: input
+ *@n: value in byte
+ *Return: return s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+	s[i] = b;
+
+	return (s);
+}
